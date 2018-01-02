9ced0414f9b9ae639c163d8425c715dc21fe9941
Hubert Dulay <hdulay@cloudera.com>
2018-01-02 09:45:32 -0500

    adding env variables to the director configuration

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Apache Spot (Incubating) in 30 minutes
-This project will build 
+This project will build a Cloudera EDH 5.12 cluster on AWS using Cloudera Director.
--- gateway/start.all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "running netflow"
-chmod +x start.flow.sh && ./start.flow.sh
-echo "running dns"
-chmod +x start.dns.sh && ./start.dns.sh
-echo "running proxy"
-chmod +x start.proxy.sh && ./start.proxy.sh
